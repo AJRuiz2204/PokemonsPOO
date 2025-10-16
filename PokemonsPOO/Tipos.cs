4bcd8bb693229e9108575a7ba844ac196ecc329b
AJRuiz2204 <ajruiz2204@gmail.com>
2025-10-16 16:37:21 -0600

    Agregar archivos de proyecto.

---
+++ PokemonsPOO/Tipos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonsPOO
+{
+    public enum TipoPokemon
+    {
+        Normal,
+        Fuego,
+        Agua,
+        Planta,
+        Electrico,
+        Hielo,
+        Lucha,
+        Veneno,
+        Tierra,
+        Volador,
+        Psiquico,
+        Bicho,
+        Roca,
+        Fantasma,
+        Dragon,
+        Acero,
+        Siniestro,
+        Hada
+    }
+}
+
